6718fb4a8ea03d810ec0a8d0b2ecd49ab2905cb8
juk85 <untaljuk@digitaldealers.mx>
2015-05-19 19:04:35 -0500

    capshi-sitio

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,13 @@
 $(document).on("ready",
 	function(){
 		$(".icono-terapia").on("click",
-			function(){
-				$("cierre-ventana").css({display: "block"});
-			});
-	});
+			function(e){
+				e.preventDefault();
+				$(".info-terapias").css({display: "block"});
+		});
+		$(".im-cierre").on("click",
+			function(e){
+				e.preventDefault();
+				$(".info-terapias").css({display: "none"});
+		});	
+});
