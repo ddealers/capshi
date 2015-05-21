929cbbfaa78eb64ea3112f4e0f7115aecc77e52f
juk85 <untaljuk@digitaldealers.mx>
2015-05-20 22:00:45 -0500

    capshi-formulario

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ $(document).on("ready",
 			function(e){
 				e.preventDefault();
 				$(".terapia-individual").css({display: "block"});
-		});
+		    });
 		$(".im-cierre").on("click",
 			function(e){
 				e.preventDefault();
@@ -20,14 +20,115 @@ $(document).on("ready",
 				e.preventDefault();
 				$(".terapiadepareja").css({display: "none"});
 			});
-		$(".boton-infantil").on("click",
+		$(".boton-terapia-infantil").on("click",
 			function(e){
+				console.log("click");
 				e.preventDefault();
-				$(".terapia-infantil").css({display: "block"});
+				$(".ter-infa-juvenil").css({display: "block"});
 			});
 		$(".im-cierre").on("click",
 			function(e){
 				e.preventDefault();
-				$(".terapia-infantil").css({display: "none"});
+				$(".ter-infa-juvenil").css({display: "none"});
+			});
+		$(".boton-familia").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-familiar").css({display: "block"});
+			});
+		$(".im-cierre").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-familiar").css({display: "none"});
+			});
+		$(".boton-acupuntura").on("click",
+			function(e){
+				e.preventDefault();
+				$(".acupuntura").css({display: "block"});
+			});
+		$(".im-cierre").on("click",
+			function(e){
+				e.preventDefault();
+				$(".acupuntura").css({display: "none"});
+			});
+		$(".boton-bioenergetica").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-bio").css({display: "block"});
+			});
+		$(".im-cierre").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-bio").css({display: "none"});
+			});
+		$(".boton-auri").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-auri").css({display: "block"});
+			});
+		$(".im-cierre").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-auri").css({display: "none"});
+			});
+		$(".boton-masaje").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-masaje").css({display: "block"});
+			});
+		$(".im-cierre").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-masaje").css({display: "none"});
+			});
+		$(".boton-constelaciones").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-constelaciones").css({display: "block"});
+			});
+		$(".im-cierre").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-constelaciones").css({display: "none"});
+			});
+		$(".boton-herbolaria").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-herbolaria").css({display: "block"});
+			});
+		$(".im-cierre").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-herbolaria").css({display: "none"});
+			});
+		$(".boton-zen").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-meditacion").css({display: "block"});
+			});
+		$(".im-cierre").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-meditacion").css({display: "none"});
+			});
+		$(".boton-floral").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-floral").css({display: "block"});
+			});
+		$(".im-cierre").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-floral").css({display: "none"});
+			});
+		$(".boton-aromaterapia").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-aromaterapia").css({display: "block"});
+			});
+		$(".im-cierre").on("click",
+			function(e){
+				e.preventDefault();
+				$(".ter-aromaterapia").css({display: "none"});
 			});
 });
